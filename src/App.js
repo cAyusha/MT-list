f03ec4aa19542f7849f1290385f8c8c72c57737f
cAyusha <aayushachaulagain544@gmail.com>
2021-11-20 13:56:56 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
 const medicalTreatmentListAsJson = 
 '[{"treatId":"1256", "treatCourseId": "24120","type":"A", "category":"cancer", "startDate": "20/23/20","name": "dash"}]'
 
+'[{"treatId":"234", "treatCourseId": "asd","type":"aq", "category":"sf", "startDate": "20/23/21","name": "hash"}]'
+
+
 
 
 
