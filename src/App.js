596c75162a8a56a837d406762ae47605c67d31ac
cAyusha <aayushachaulagain544@gmail.com>
2021-11-20 14:14:14 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import MedicalTreatmentList from './MedicalTreatmentList';
 
 export default function App() {
 const medicalTreatmentListAsJson = 
-'[{"treatId":"1256", "treatCourseId": "24120","type":"A", "category":"cancer", "startDate": "20/23/20","name": "dash"}]'
-
-'[{"treatId":"234", "treatCourseId": "asd","type":"aq", "category":"sf", "startDate": "20/23/21","name": "hash"}]'
-
-
-
+'[ {"treatId": "21420", "treatCourseId":" 683", "type": " Major", "category": " Cancer ", "name": " Alex Nepali", "startDate": " 19/11/21"} ,' +
+    '{"treatId": "403", "treatCourseId":" 782", "type": " Minor", "category": " Fever", "name": " James Shah", "startDate": "  23/10/21"} ,' +
+    '{"treatId": "404", "treatCourseId":" 231", "type": " Major", "category": " Kidney Failure", "name": " Sarah Gurung", "startDate": " 16/08/19"} ,' +
+    '{"treatId": "405", "treatCourseId":" 3895", "type": " Major", "category": " Bipolar Disorder", "name": " Lucy Ramsey", "startDate": " 19-08-2019"} ,' +
+    '{"treatId": "406", "treatCourseId":" 1093", "type": " Major", "category": " Brain Injury", "name": " Anda Tamang", "startDate": " 11-11-2019"} ]';
 
 
 
--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -7,7 +7,7 @@ const medicalTreatmentListJ = props.json;
 const medicaltreatmentlist = JSON.parse(medicalTreatmentListJ);
 const ListMedicalTreatmentList = medicaltreatmentlist.map((medicalTreatment)=>
 <li>
-  <MedicalTreatment treatid = {medicalTreatment.treatid} treatCourseId = {medicalTreatment.treatCourseId}type ={medicalTreatment.type}category={medicalTreatment.category}name ={medicalTreatment.name}startDate= {medicalTreatment.startDate}/>
+  <MedicalTreatment treatId = {medicalTreatment.treatId} treatCourseId = {medicalTreatment.treatCourseId}type ={medicalTreatment.type}category={medicalTreatment.category}name ={medicalTreatment.name}startDate= {medicalTreatment.startDate}/>
 
   </li>);
   return (
