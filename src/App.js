1e1c2ee3f9281357534b7d6a1883e60e7c5fa7f4
cAyusha <aayushachaulagain544@gmail.com>
2021-12-03 12:06:06 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ const medicalTreatmentListAsJson =
     '{"treatId": "404", "treatCourseId":" 2311", "type": " Major", "category": " Kidney Failure", "name": " Sarah Gurung", "startDate": " 16/08/19"} ,' +
     '{"treatId": "405", "treatCourseId":" 3895", "type": " Major", "category": " Bipolar Disorder", "name": " Lucy Ramsey", "startDate": " 19/08/2019"} ,' +
     '{"treatId": "406", "treatCourseId":" 1093", "type": " Major", "category": " Brain Injury", "name": " Anda Tamang", "startDate": " 11/11/2019"} ]';
+const data = JSON.parse (medicalTreatmentListAsJson);
 
 
-
-  return (
-    <div>
+   return (
+     <div>
       <h1 style={{color: "blue", backgroundColor: "white"}}>List of Medical Records</h1>
-      
       <MedicalTreatmentList json = {medicalTreatmentListAsJson}/>
+      <h1> Search Results </h1>
+
+      {data.filter(record => record.type== "Minor").map (filteredRecord => ( <li> {filteredRecord.name}</li>))}
     </div>
   );
 }
