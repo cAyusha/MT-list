1a93b7fcc3dcb20bd89ab14d6f525dd631c7d83b
cAyusha <aayushachaulagain544@gmail.com>
2021-12-04 14:34:05 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import "./style.css";
 import MedicalTreatmentList from './MedicalTreatmentList';
 
 export default function App() {
-const medicalTreatmentListAsJson = 
-'[ {"treatId": "401", "treatCourseId":" 6832", "type": " A", "category": " Cancer ", "name": " Alex Nepali", "startDate": " 19/11/21"} ,' +
-    '{"treatId": "403", "treatCourseId":" 7821", "type": " R", "category": " Fever", "name": " James Shah", "startDate": "  23/10/21"} ,' +
-    '{"treatId": "404", "treatCourseId":" 2311", "type": " D", "category": " Kidney Failure", "name": " Sarah Gurung", "startDate": " 16/08/19"} ,' +
-    '{"treatId": "405", "treatCourseId":" 3895", "type": " T", "category": " Bipolar Disorder", "name": " Lucy Ramsey", "startDate": " 19/08/2019"} ,' +
-    '{"treatId": "406", "treatCourseId":" 1093", "type": " U", "category": " Brain Injury", "name": " Anda Tamang", "startDate": " 11/11/2019"} ]';
+  const medicalTreatmentListAsJson = 
+  '[ {"treatId": "401", "treatCourseId":" 6832", "type": " Major", "category": " Cancer ", "name": " Alex Nepali", "startDate": " 19/11/21"} ,' +'{"treatId": "403", "treatCourseId":" 7821", "type": " Minor", "category": " Fever", "name": " James Shah", "startDate": "  23/10/21"} ,' +'{"treatId": "404", "treatCourseId":" 2311", "type": " Major", "category": " Kidney Failure", "name": " Sarah Gurung", "startDate": " 16/08/19"} ,' +'{"treatId": "405", "treatCourseId":" 3895", "type": " Major", "category": " Bipolar Disorder", "name": " Lucy Ramsey", "startDate": " 19/08/2019"} ,' +'{"treatId": "406", "treatCourseId":" 1093", "type": " Major", "category": " Brain Injury", "name": " Anda Tamang", "startDate": " 11/11/2019"} ]';
+  
 const data = JSON.parse (medicalTreatmentListAsJson);
 
 
